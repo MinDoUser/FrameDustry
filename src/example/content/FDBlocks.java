64f612b0203233cfaab4e3f8d760bc6d5a3e1220
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-20 18:49:47 +0100

    Create FDBlocks.java

---
+++ src/example/content/FDBlocks.java
@@ -0,0 +1,42 @@
+package example.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.ctype.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.entities.bullet.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.meta.*;
+import mindustry.world.draw.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class CreatiiveBlocks implements ContentList {
+  public static Block 
+    //walls
+    leadwall;
+  	@Override
+	public void load() {
+    
+  		leadwall = new Wall("lead-wall") {{
+			requirements(Category.defense, with(Items.lead, 6));
+			localizedName = "Lead Wall";
+			health = 350;
+			size = 1;
+		}};
+  }
+}
