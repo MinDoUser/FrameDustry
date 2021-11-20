8975a7fe39aa09dfe6e9697c298a6c794af437f2
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-20 18:23:39 +0100

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -18,11 +18,11 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("Welcome!");
+                dialog.cont.add("Welcome to my first JAR mods. \n I am sorry if there are any errors.").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                //dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.button("Let's play!", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
