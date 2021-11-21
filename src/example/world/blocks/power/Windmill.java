b5ff4e2c3816bbdde65e141fb19ef434e74742b7
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-21 12:58:24 +0100

    Added Windmill class
    Testing `Windmill.java`

---
+++ src/example/world/blocks/power/Windmill.java
@@ -0,0 +1,39 @@
+package example.world.blocks.power;
+
+import arc.math.*;
+import arc.struct.*;
+import mindustry.world.meta.*;
+import arc.util.Time;
+
+import static mindustry.Vars.*;
+
+public class Windmill extends PowerGenerator {
+  public float rotorSpeed = 4;
+  public TextureRegion rotorRegion, blockRegion;
+      public Windmill(String name){
+        super(name);
+        //Creates a low priority for units...
+        flags = EnumSet.of();
+        envEnabled = Env.any;
+    }
+  	public void load() {
+		blockRegion = Core.atlas.find(name);
+		rotorRegion = Core.atlas.find(name + "-rotor");
+	}
+      @Override
+    public void setStats(){
+        super.setStats();
+        stats.remove(generationType);
+        stats.add(generationType, powerProduction * 60.0f, StatUnit.powerSecond);
+    }
+      public class WindmillBiuld extends GeneratorBuild{
+      @Override
+        public void updateTile(){
+            productionEfficiency = 1f;//*wind; Need to create a map setting 'Wind' first...
+        }
+        public void draw(Block b) {
+          		Draw.rect(rotorRegion, b.x, b.y, Time.time * rotorSpeed);
+		          Draw.rect(blockRegion, b.x, b.y);
+        }
+      }
+}
