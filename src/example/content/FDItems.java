44209b14aeeda9f5c52e1a8fe7f721cac0fff8a5
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-21 14:38:55 +0100

    Create FDItems.java

---
+++ src/example/content/FDItems.java
@@ -0,0 +1,17 @@
+package example.content;
+
+import arc.graphics.Color;
+import mindustry.ctype.ContentList;
+import mindustry.type.Item;
+
+public class FDItems extends ContentList {
+	public static Item 
+	exite;
+  
+  	@Override
+	public void load(){
+		exite = new Item("exite", Color.valueOf("add8e6")) {{
+			localizedName = "Exite";
+			cost = 1.1;
+		}};
+  }
