58de955de1f0451acf19022689b161b5a1947c6d
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-21 15:22:33 +0100

    Update FDItems.java

--- src/example/content/FDItems.java
+++ src/example/content/FDItems.java
@@ -8,7 +8,7 @@ public class FDItems implements ContentList {
 	public static Item 
 	exite;
   
-  	//@Override
+  	@Override
 	public void load(){
 		exite = new Item("exite", Color.valueOf("add8e6")) {{
 			localizedName = "Exite";
