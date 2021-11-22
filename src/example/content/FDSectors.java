c22d2d90097bcce84b194c1ada0a7eeab7368f6c
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-22 16:22:18 +0100

    Update FDSectors.java

--- src/example/content/FDSectors.java
+++ src/example/content/FDSectors.java
@@ -5,12 +5,12 @@ import mindustry.type.*;
 
 //import static example.content.*;
 public class FDSectors implements ContentList{
-    public static SectorPreset gluonFields;
+    public static SectorPreset coast;
       @Override
     public void load(){
         //region Gluon
 
-        gluonFields = new SectorPreset("gluonFields", FDPlanets.gluon, 15){{
+        gluonFields = new SectorPreset("coast", FDPlanets.gluon, 15){{
             alwaysUnlocked = true;
             addStartingItems = true;
             captureWave = 10;
