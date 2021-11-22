066eacb55a5632f568dd9bc797b17cd81de61cdd
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2021-11-22 17:41:15 +0100

    Create ArcBulletType.java

---
+++ src/example/entity/bullets/ArcBulletType.java
@@ -0,0 +1,26 @@
+package example.entity.bullets;
+
+import mindustry.content.Fx;
+import mindustry.entities.bullet.BulletType;
+import mindustry.gen.Bullet;
+import mindustry.entities.bullet.BasicBulletType;
+
+public class ArcBulletType extends BasicBulletType{
+  public int lightingAmount = 10;
+  pulbic float lightningDamage = 60;
+		public ArcBulletType(float speed, float damage, String bulletSprite, int lightnings, float lightningD) {
+		super(speed, damage, bulletSprite);
+    this.lightningAmount = lightnings;
+    this.lightningDamage = lightningD;
+	}
+
+	@Override public void draw(Bullet b){}
+	@Override public void drawLight(Bullet b){}
+  @Override
+  public void update(Bullet b){
+    for(int x; x<=lightningAmount; x++){
+    Lightning.create(b.team, Color.valueOf("ffffff"), lightningDamage, b.x, b.y, 40);
+    }
+    super.update(b);
+  }
+}
