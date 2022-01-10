87da092a9b83da15c42c7c0d167149bc10b3c8e1
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-01-10 16:42:51 +0100

    Create WaveInfoMessage.java

---
+++ src/example/world/blocks/logic/WaveInfoMessage.java
@@ -0,0 +1,24 @@
+package example.world.blocks.logic;
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.util.*;
+import arc.util.*;
+import mindustry.gen.*;
+import mindustry.*;
+import mindustry.world.blocks.logic.*;
+
+//Creates an type that makes info about wave
+
+public class WaveInfoMessage extends MessageBlock {
+    public WaveInfoMessage(String name) {
+        super(name);
+        update = true;
+    }
+    public class WaveInfoMessageBiuld extends MessageBuild {
+              @Override
+        public void updateTile() {
+           message.append("You are currently in wave: [stat]"+Vars.state.wave+"[white].");
+        }
+    }
+}
