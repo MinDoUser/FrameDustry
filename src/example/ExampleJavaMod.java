2977ef0e1037345a4bfabac142716a29dcb1b570
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-01-17 13:08:45 +0100

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -9,33 +9,38 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
-
+import mindustry.ui.dialogs.BaseDialog;
+import mindustry.ui.Styles;
+//Yep, it's not the best name, but everthing else just didn't work...
 public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("Welcome!");
-                dialog.cont.add("Welcome to my first JAR mods. \n I am sorry if there are any errors.").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                //dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("Let's play!", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+        Log.info("Loaded[stat] FrameDustry[] constructor.");
+        Log.info("So, yeah, let's have fun together, hopefully... ?");
     }
+    public static void startLog(){
+        	BaseDialog dialog = new BaseDialog("");
 
+    dialog.buttons.defaults().size(210, 64);
+   dialog.closeOnBack();
+       dialog.cont.pane(inner -> {
+
+        inner.pane(table -> {
+        table.add("[white]<< Loaded Framedustry >>", Styles.techLabel).row();
+        table.row();
+	    table.add("This Mod is still in Development! \n It adds some intresting things and other senceless stuff. \n Have Fun!").left().growX().wrap().width(620).maxWidth(620).pad(4).labelAlign(Align.left);
+	    table.row();
+        table.add("[cyan]> By lol02 <", Styles.techLabel).row();
+    })()).grow().center().maxWidth(620);
+    dialog.show();
+    }
     @Override
     public void loadContent(){
         new FDBlocks().load();
         new FDWeather().load();
         new FDItems().load();
-        new FDPlanets().load();
-        new FDSectors().load();
+        //new FDPlanets().load();
+        //new FDSectors().load();
         Log.info("Loaded all Java files.");
     }
 
